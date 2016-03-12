f64b1efa88e6b62a70e63feb8f19ef3b2b357ea2
kiangmusa <kiangmusa23@gmail.com>
2016-03-12 21:42:10 +0700

    Update Ruangan.java

--- PasienInapRumahSakit/src/pasieninaprumahsakit/Ruangan.java
+++ PasienInapRumahSakit/src/pasieninaprumahsakit/Ruangan.java
@@ -5,49 +5,65 @@
  */
 package pasieninaprumahsakit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author AngkyMusa
  */
 public class Ruangan {
-    private PasienInap[] daftarPasien ;
+
     private String noRuang;
     private int nPasien;
     private int nKapasitas;
-    
-    public Ruangan(String noRuang, int n){
-        this.noRuang=noRuang ;
-        this.nKapasitas=n;
-        daftarPasien=new PasienInap[nKapasitas];
+    private List<PasienInap> daftarPasien = new ArrayList<PasienInap>();
+
+    public Ruangan(String noRuang, int n) {
+        this.noRuang = noRuang;
+        this.nKapasitas = n;
     }
-    public void tambahPasienInap(PasienInap P, Dokter d){
-        if(nPasien<=daftarPasien.length) {
-            daftarPasien[nPasien]=P;
-            daftarPasien[nPasien].setDokter(d);
+
+    public void tambahPasienInap(Pasien P, Dokter d) {
+        if (nPasien < nKapasitas) {
+            daftarPasien.add(new PasienInap(P));
+            daftarPasien.get(nPasien).setDokter(d);
             nPasien++;
-        }
-        else {
+        } else {
             System.out.println("Ruangan penuh");
         }
+    }
+
+    public PasienInap getPasienInap(int n) {
+        return daftarPasien.get(n);
     }
 
     public String getNoRuang() {
         return this.noRuang;
     }
-
-    public void setNoRuang(String noRuang) {
-        this.noRuang = noRuang;
-    }
-
     public int getnPasien() {
         return nPasien;
     }
-
-    public void setnPasien(int nPasien) {
-        this.nPasien = nPasien;
+    public int getKapasitas() {
+        return daftarPasien.size();
     }
-    public int getKapasitas(){
-        return daftarPasien.length-nPasien;
+     public int getNKapasitas() {
+        return nKapasitas;
     }
-    
+    public PasienInap getPasienInapById(String id) {
+        PasienInap Pi = null;
+        for (int i = 0; i < daftarPasien.size(); i++) {
+            if (daftarPasien.get(i).getPasien().getIdPasien().equals(id)) {
+                Pi = daftarPasien.get(i);
+            }
+        }
+        return Pi;
+    }
+    public void deletePasienInapById(String id) {
+        for (int i = 0; i < daftarPasien.size(); i++) {
+            if (daftarPasien.get(i).getPasien().getIdPasien().equals(id)) {
+                daftarPasien.remove(i);
+            }
+        }
+    }
 }
